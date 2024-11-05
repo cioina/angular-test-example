0d8149837389fdd952b4590c80c4d72411ab0d20
cioina <alexei_cioina@yahoo.com>
2024-11-05 10:58:23 -0800

    update all

--- version-1/+state/auth.reducer.ts
+++ version-1/+state/auth.reducer.ts
@@ -1,8 +1,3 @@
-/**
- * Use of this source code is governed by an MIT-style license that can be
- * found in the LICENSE file at https://github.com/NG-ZORRO/ng-zorro-antd/blob/master/LICENSE
- */
-
 import { Action, createReducer, on } from '@ngrx/store';
 import { Profile, Version, User } from '@zorro-example-app/api';
 
--- version-1/+state/auth.selectors.ts
+++ version-1/+state/auth.selectors.ts
@@ -1,8 +1,3 @@
-/**
- * Use of this source code is governed by an MIT-style license that can be
- * found in the LICENSE file at https://github.com/NG-ZORRO/ng-zorro-antd/blob/master/LICENSE
- */
-
 import { createFeatureSelector, createSelector } from '@ngrx/store';
 
 import { Auth } from './auth.reducer';
